f9bbe05ef1a7d5559d5a9ba8bfd34b03b8fc359e
Michael Tsao <caoxiang@yeah.net>
2016-11-17 16:44:31 +0800

    modify redis common & add SortList

--- RedisCommon.php
+++ RedisCommon.php
@@ -9,32 +9,53 @@
 namespace mycompany\common;
 
 use Yii;
-use yii\redis\Cache;
-use yii\redis\Connection;
+use yii\base\Object;
 
-class RedisCommon extends Connection
+class RedisCommon extends Object
 {
+    public $prefix = null;
+    public $redis = null;
+
+    public function init()
+    {
+        if (!$this->redis && isset(Yii::$app->redis)) {
+            $this->redis = Yii::$app->redis;
+        }
+    }
+
+    public function buildKey($key)
+    {
+        return $this->prefix . $key;
+    }
+
     /**
      * 封装hmset方法
      * @param $key
      * @param array $info
      * @return bool
      */
-    static public function setHash_Array($key,$info=[]){
-        if($info===[] || !is_array($info))  ApiException::Msgs(99, '使用 setHash_Array 方法 参数 info 不是数组');
-        foreach ($info as $k=>$v){
+    static public function setHash_Array($key, $info = [])
+    {
+        if ($info === [] || !is_array($info)) ApiException::Msgs(99, '使用 setHash_Array 方法 参数 info 不是数组');
+        foreach ($info as $k => $v) {
             Yii::$app->redis->hmset($key, $k, $v);
         }
         return true;
     }
 
-    static public function hashToArray($r){
-        $d = [];$n = 1;$l='';
-        foreach ($r as $k=>$v){
-            if($n){
-                $d[$v]='';$l=$v;$n=0;
-            }else{
-                $d[$l]=$v;$n=1;
+    static public function hashToArray($r)
+    {
+        $d = [];
+        $n = 1;
+        $l = '';
+        foreach ($r as $k => $v) {
+            if ($n) {
+                $d[$v] = '';
+                $l = $v;
+                $n = 0;
+            } else {
+                $d[$l] = $v;
+                $n = 1;
             }
         }
         return $d;
---
+++ SortList.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caoxiang
+ * Date: 2016/11/17
+ * Time: 下午2:42
+ */
+
+namespace mycompany\common;
+
+class SortList extends RedisCommon
+{
+    public $key = null;
+
+    public function create($data)
+    {
+        $this->redis->DEL([$this->key]);
+        foreach ($data as $item => $value) {
+            $this->redis->ZADD([$this->buildKey($this->key), $value, $item]);
+        }
+    }
+}
